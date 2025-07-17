54ae8cf17666c529254914697c2349ad924ef787
Antoni Alvarez <t.alvarezpascual@gmail.com>
2025-07-17 22:41:17 +0200

    Expose port 8080 to make requests

--- app/src/Adapter/Framework/Http/Controller/FoodController.php
+++ app/src/Adapter/Framework/Http/Controller/FoodController.php
@@ -5,26 +5,50 @@ declare(strict_types=1);
 namespace App\Adapter\Framework\Http\Controller;
 
 use App\Adapter\Framework\Http\Request\FoodFilterRequestDTO;
-use App\Application\Service\FoodFetcher;
-use App\Application\Service\FoodUnitConverter;
+use App\Application\UseCase\FoodAdd;
 use App\Application\UseCase\FoodList;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\Routing\Attribute\Route;
+use Throwable;
+
+use function sprintf;
 
 final class FoodController extends AbstractController
 {
     public function __construct(
         private readonly FoodList $foodList,
+        private readonly FoodAdd $foodAdd,
+        private readonly LoggerInterface $logger,
     ) {}
 
     #[Route('/food', name: 'List food', methods: [Request::METHOD_GET])]
-    public function listFood(#[MapQueryString] FoodFilterRequestDTO $requestDTO): Response
+    public function listFood(#[MapQueryString] FoodFilterRequestDTO $request): Response
     {
-        $foodItems = $this->foodList->execute($requestDTO->type, $requestDTO->unit);
+        try {
+            $foodItems = $this->foodList->execute($request->type, $request->unit);
+        } catch (Throwable $t) {
+            $this->logger->error(sprintf('[FoodController] Error fetching food items <%s>', $t->getMessage()));
+
+            throw $t;
+        }
 
         return $this->json($foodItems);
+    }
+
+    #[Route('/food', name: 'Add food item', methods: [Request::METHOD_POST])]
+    public function addFood(#[MapRequestPayload] FoodCreateRequestDTO $request): Response
+    {
+        try {
+            $this->foodAdd->execute($request->id, $request->name, $request->type, $request->quantity, $request->unit);
+        } catch (Throwable $t) {
+            $this->logger->error(sprintf('[FoodController] Error adding food item: <%s>', $t->getMessage()));
+
+            throw $t;
+        }
+
+        return $this->json(Response::HTTP_CREATED);
     }
 }
